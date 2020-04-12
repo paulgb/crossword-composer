2364a15568e72f4cdb08654573be111f0d0d2fa0
Paul Butler <paulgb@gmail.com>
2020-04-12 12:41:55 -0400

    dimension drop-down and favicon

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,12 +1,30 @@
 <script>
 import Crossword from './Crossword.svelte'
 
+const allowedDimensions = [
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
+	10,
+	11,
+	12
+]
+
 $: dimension = 5;
 </script>
 
 <div class="main">
 	<Crossword dimension={parseInt(dimension)} />
-	<label>Height/width: <input bind:value={dimension} /></label>
+	<label>Size: <select bind:value={dimension}>
+	{#each allowedDimensions as ad}
+	<option value={ad}>{ad} x {ad}</option>
+	{/each}
+	</select>
+	</label>
 </div>
 
 <style>
